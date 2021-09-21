b803b2de25e8b096932323c08efd27abd532c3bc
YagoFdez <yago.fmiranda@gmail.com>
2021-09-22 00:37:07 +0200

    Buttons and state counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{counter}</div>
+        <div className="row">
+          <button className="btcounter" onClick={() => setCounter(counter + 1)}>+</button><button className="btcounter" onClick={() => setCounter(counter - 1)}>-</button>
+        </div>
       </header>
     </div>
   );
